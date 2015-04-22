3358bbf428b3e0b1bdf261ac3d1dfed6c2bc9db3
Lex Robinson <lexi@lexi.org.uk>
2015-04-22 21:03:11 +0100

    Initial Commit

---
+++ gamemode/init.lua
@@ -0,0 +1,17 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include( "shared.lua" )
+
+
+function GM:InitPostEntity()
+	game.ConsoleCommand("sv_accelerate 100\n");
+	game.ConsoleCommand("sv_airaccelerate 100\n");
+end
+
+function GM:PlayerSpawn(ply)
+	player_manager.SetPlayerClass( ply, "class_default" )
+	self.BaseClass.PlayerSpawn(self, ply)
+	print("Hi!", ply)
+end
